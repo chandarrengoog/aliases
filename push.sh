beebb9dae462f0cbb9381c5fba15e7ff438dcdb8
Darren Chan <chandarren@google.com>
2023-10-30 18:29:15 -0400

    Autoexport aliases.

--- push.sh
+++ push.sh
@@ -7,6 +7,9 @@ cd ~/aliases
 if ! git ls-files --other --directory --exclude-standard | sed q1 > /dev/null || ! git diff-files --quiet; then
   git add .
   git commit -m 'Autoexport aliases.'
+fi
+
+if ! git ls-remote origin | grep (git rev-parse HEAD); then
   git push origin main
 fi
 
